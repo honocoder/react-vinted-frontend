fe5ca8ba8f810e3bee5d5be42f7a9c5515d93eb0
jimymltta <marlettajimy@gmail.com>
2021-08-03 23:52:29 +0200

    test

--- src/containers/Payment.js
+++ src/containers/Payment.js
@@ -1,32 +1,32 @@
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import { useLocation } from "react-router";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { useLocation } from "react-router";
 
-// Component import
-import CheckoutForm from "../components/CheckoutForm";
+// // Component import
+// import CheckoutForm from "../components/CheckoutForm";
 
-const stripePromise = loadStripe(
-  "pk_test_51JKNh7DBzgsanfUiD0u2eVkWvQ4J6bRZDP0o4JUlFSHYuYowCd8hIlUOGAmL2G53qPru270ETTyylYbxlYqSi57a00YswXevj8"
-);
+// const stripePromise = loadStripe(
+//   "pk_test_51JKNh7DBzgsanfUiD0u2eVkWvQ4J6bRZDP0o4JUlFSHYuYowCd8hIlUOGAmL2G53qPru270ETTyylYbxlYqSi57a00YswXevj8"
+// );
 
-const Payment = () => {
-  const location = useLocation();
-  // const { userToken, users._id, product_name, product_price } = location.state;
+// const Payment = () => {
+//   const location = useLocation();
+//   // const { userToken, users._id, product_name, product_price } = location.state;
 
-  return (
-    <div>
-      <div>
-        <Elements stripe={stripePromise}>
-          <CheckoutForm
-          //   userToken={users.token}
-          //   userId={users._id}
-          //   productTitle={product_name}
-          //   productPrice={product_price}
-          />
-        </Elements>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div>
+//         <Elements stripe={stripePromise}>
+//           <CheckoutForm
+//           //   userToken={users.token}
+//           //   userId={users._id}
+//           //   productTitle={product_name}
+//           //   productPrice={product_price}
+//           />
+//         </Elements>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Payment;
+// export default Payment;
